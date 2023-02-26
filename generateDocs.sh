ed4b0ba3244438229100d7f353e7398fd79c2d82
Max-Joseph Krempl <krempl@in.tum.de>
2023-02-26 23:37:19 +0100

    Added deterministic key order to JSON files of generated documentation

--- generateDocs.sh
+++ generateDocs.sh
@@ -9,3 +9,12 @@ xcodebuild docbuild -scheme IntGeometry -destination "platform=macOS" -derivedDa
 doccarchive=$(find "$tempDerivedDataPath" -type d -name "*.doccarchive") || exit 1
 $(xcrun --find docc) process-archive transform-for-static-hosting "$doccarchive" --output-path "$outputPath" --hosting-base-path "$hostingBasePath"
 rm -r "$tempDerivedDataPath"
+
+echo "Sorting JSON keys..."
+find "$outputPath"/data "$outputPath"/index -name "*.json" -print0 | while read -d $'\0' file
+do
+	file_new="${file}_new"
+	jq --sort-keys --compact-output . "$file" > "$file_new"
+	#jq --sort-keys --compact-output . "$file" | perl -0777 -pe 's{"(?:\\.|.)*?"(\s*:)?}{ $1 ? $& : $& =~ s{/}{\\/}gr }ge' > "$file_new"  # Keep "\/" escaped forward slash in string values
+	mv "$file_new" "$file"
+done
